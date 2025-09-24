6b75bc0dec1c8ca30ba356f6152730780080b03b
vorlie <vorlie0961@gmail.com>
2025-09-24 17:04:12 +0200

    asd

--- src/settingsdialog.h
+++ src/settingsdialog.h
@@ -22,8 +22,8 @@ private slots:
     void on_executableBrowseButton_clicked();
     void on_replaysBrowseButton_clicked();
     void on_versionBrowseButton_clicked();
-    void on_buttonBox_accepted();  // Add this to match UI signal
-    void on_buttonBox_rejected();  // Add this to match UI signal
+    void on_buttonBox_accepted();
+    void on_buttonBox_rejected();
 
 private:
     Ui::SettingsDialog *ui;
